802804209cbcf3b3b2ec71058d7f3c7511ee24f7
yjh <dathea87@gmail.com>
2022-09-07 17:58:11 +0900

    Daemon test

--- src/main/java/org/paasta/container/terraman/api/common/repository/AccountRepository.java
+++ src/main/java/org/paasta/container/terraman/api/common/repository/AccountRepository.java
@@ -2,6 +2,7 @@ package org.paasta.container.terraman.api.common.repository;
 
 import org.paasta.container.terraman.api.common.model.AccountModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
@@ -9,5 +10,5 @@ import javax.transaction.Transactional;
 @Repository
 @Transactional
 public interface AccountRepository extends JpaRepository<AccountModel, Long> {
-    AccountModel findById(int id);
+    AccountModel findById(@Param("id") int id);
 }
