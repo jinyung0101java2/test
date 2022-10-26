588e76a0d95429e4a5003bfcf69ee6e8ceb10ad6
hoon77 <slokang@naver.com>
2022-10-26 16:45:55 +0900

    Add privateRepository link

--- src/main/java/org/paasta/container/platform/web/ui/privateRepositoryInfo/PrivateRepositoryInfoController.java
+++ src/main/java/org/paasta/container/platform/web/ui/privateRepositoryInfo/PrivateRepositoryInfoController.java
@@ -3,7 +3,10 @@ package org.paasta.container.platform.web.ui.privateRepositoryInfo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.paasta.container.platform.web.ui.common.ConstantsUrl;
+import org.paasta.container.platform.web.ui.common.PropertyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -18,6 +21,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PrivateRepositoryInfoController {
     private static final String BASE_URL = "privateRepositoryInfo/";
+    private final PropertyService propertyService;
+
+    @Autowired
+    public PrivateRepositoryInfoController(PropertyService propertyService) {
+        this.propertyService = propertyService;
+    }
+
 
     /**
      * Private Repository Info 페이지 이동(Go to the Private Repository Info page)
@@ -25,8 +35,9 @@ public class PrivateRepositoryInfoController {
      * @return the view
      */
     @ApiOperation(value = "Private Repository Info 페이지 이동(Go to the Private Repository Info page)", nickname = "getPrivateRepositoryInfo")
-    @GetMapping(value = ConstantsUrl.URI_CP_INFO_PRIVATE_REPOSITORY )
-    public String getPrivateRepositoryInfo() {
+    @GetMapping(value = ConstantsUrl.URI_CP_INFO_PRIVATE_REPOSITORY)
+    public String getPrivateRepositoryInfo(Model model) {
+        model.addAttribute("privateRepositoryUrl", propertyService.getPrivateRepositoryUrl());
         return BASE_URL + "privateRepositoryInfo";
     }
 }
