22a7c2469c0ff1ae6985f72eb859d679578a6735
yjh <dathea87@gmail.com>
2022-11-02 11:23:18 +0900

    [2022-11-02] modified path

--- src/main/java/org/paasta/container/terraman/api/common/service/TfFileService.java
+++ src/main/java/org/paasta/container/terraman/api/common/service/TfFileService.java
@@ -110,7 +110,7 @@ public class TfFileService {
 
         if(StringUtils.equals(resultFile, Constants.RESULT_STATUS_SUCCESS)) {
             if(!StringUtils.isBlank(idRsa) && !StringUtils.isBlank(host)) {
-                File uploadfile = new File( TerramanConstant.FILE_PATH(TerramanConstant.MOVE_DIR_CLUSTER_FILE_UPLOAD(clusterId, processGb)) ); // 파일 객체 생성
+                File uploadfile = new File( TerramanConstant.FILE_PATH(TerramanConstant.MOVE_DIR_CLUSTER(clusterId, processGb)) ); // 파일 객체 생성
                 commandService.sshFileUpload(TerramanConstant.MOVE_DIR_CLUSTER(clusterId, processGb), host, idRsa, uploadfile, TerramanConstant.DEFAULT_USER_NAME);
             }
             resultCode = commandService.execCommandOutput(TerramanConstant.INSTANCE_COPY_COMMAND(pod, clusterId), "", host, idRsa, TerramanConstant.DEFAULT_USER_NAME);
