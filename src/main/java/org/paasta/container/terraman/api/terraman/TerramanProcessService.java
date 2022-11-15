e158c865dd564c2436a91cc937db1bb645522896
yjh <dathea87@gmail.com>
2022-11-15 15:03:15 +0900

    [2022-11-14] modified init container

--- src/main/java/org/paasta/container/terraman/api/terraman/TerramanProcessService.java
+++ src/main/java/org/paasta/container/terraman/api/terraman/TerramanProcessService.java
@@ -430,6 +430,7 @@ public class TerramanProcessService {
 
         terramanCommandModel.setCommand("9");
         cResult = commandService.execCommandOutput(terramanCommandModel);
+        LOGGER.info("Deploy Cluster :: {}", CommonUtils.loggerReplace(cResult));
         if(StringUtils.equals(Constants.RESULT_STATUS_FAIL, cResult)) {
             clusterLogService.saveClusterLog(clusterId, mpSeq, TerramanConstant.TERRAFORM_DEPLOY_CLUSTER_ERROR);
             clusterService.updateCluster(clusterId, TerramanConstant.CLUSTER_FAIL_STATUS);
@@ -460,7 +461,7 @@ public class TerramanProcessService {
         terramanCommandModel.setUserName(TerramanConstant.DEFAULT_USER_NAME);
         terramanCommandModel.setClusterId(clusterId);
         cResult = commandService.execCommandOutput(terramanCommandModel);
-
+        LOGGER.info("Account Create :: {}", CommonUtils.loggerReplace(cResult));
         if(StringUtils.equals(Constants.RESULT_STATUS_FAIL, cResult)) {
             clusterLogService.saveClusterLog(clusterId, mpSeq, TerramanConstant.TERRAFORM_CREATE_SERVICE_ACCOUNT_ERROR);
             clusterService.updateCluster(clusterId, TerramanConstant.CLUSTER_FAIL_STATUS);
@@ -469,7 +470,7 @@ public class TerramanProcessService {
 
         terramanCommandModel.setCommand("11");
         cResult = commandService.execCommandOutput(terramanCommandModel);
-
+        LOGGER.info("Account Binding :: {}", CommonUtils.loggerReplace(cResult));
         if(StringUtils.equals(Constants.RESULT_STATUS_FAIL, cResult)) {
             clusterLogService.saveClusterLog(clusterId, mpSeq, TerramanConstant.TERRAFORM_BIND_ROLE_ERROR);
             clusterService.updateCluster(clusterId, TerramanConstant.CLUSTER_FAIL_STATUS);
@@ -478,6 +479,7 @@ public class TerramanProcessService {
 
         terramanCommandModel.setCommand("12");
         cResult = commandService.execCommandOutput(terramanCommandModel);
+        LOGGER.info("Account Secret Name :: {}", CommonUtils.loggerReplace(cResult));
         if(StringUtils.equals(Constants.RESULT_STATUS_FAIL, cResult) || StringUtils.isBlank(cResult)) {
             clusterLogService.saveClusterLog(clusterId, mpSeq, TerramanConstant.TERRAFORM_GET_SECRET_NAME_ERROR);
             clusterService.updateCluster(clusterId, TerramanConstant.CLUSTER_FAIL_STATUS);
@@ -487,6 +489,7 @@ public class TerramanProcessService {
         terramanCommandModel.setCommand("13");
         terramanCommandModel.setSecrets(cResult);
         cResult = commandService.execCommandOutput(terramanCommandModel);
+        LOGGER.info("Service Account Token :: {}", CommonUtils.loggerReplace(cResult));
         if(StringUtils.equals(Constants.RESULT_STATUS_FAIL, cResult)) {
             clusterLogService.saveClusterLog(clusterId, mpSeq, TerramanConstant.TERRAFORM_GET_CLUSTER_TOKEN_ERROR);
             clusterService.updateCluster(clusterId, TerramanConstant.CLUSTER_FAIL_STATUS);
